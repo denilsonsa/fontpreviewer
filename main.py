652492979e7e7dfeb09386e7f95ac1397ea5392f
Denilson Sá <denilsonsa@gmail.com>
2011-04-17 00:15:50 -0300

    Updated HTML generation code slightly. Added support for UTF-8 "--text".

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def render_font_to_file(fontfile, text, fontsize, antialias=True, background=Non
     else:
         surf = font.render(text, antialias, (0,0,0), background)
 
-    pngfile = os.path.splitext(fontfile)[0] + ".png"
+    pngfile = os.path.splitext(fontfile)[0] + '.png'
     pygame.image.save(surf, pngfile)
 
 
@@ -57,7 +57,7 @@ class FontMetadata(object):
         self.filename = fontfile
         self.name = short_name[0]
         self.name_family = short_name[1]
-        self.family = "/".join(family)
+        self.family = '/'.join(family)
         self.weight = modifiers[0]
         self.weight_name = describe.weightName(modifiers[0])
         if modifiers[1]:
@@ -66,10 +66,32 @@ class FontMetadata(object):
             self.italic = False
 
 
-def print_html(metadata, html_file, text=""):
-    html_file.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head>
-<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+def print_html(metadata, html_file, text=u''):
+    def html(s):
+        s = (s
+            .replace('&', '&amp;')
+            .replace('<', '&lt;')
+            .replace('>', '&gt;')
+            .replace('"', '&quot;')
+        )
+        if isinstance(s, unicode):
+            s = s.encode('utf-8', 'replace')
+        elif isinstance(s, str):
+            pass
+        else:
+            raise ValueError(
+                'What kind of type is this? type(s)={0} repr(s)={1}'
+                .format(type(s), repr(s))
+            )
+        return s
+
+    text = html(text)
+
+    html_file.write('''<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="Generator" content="fontpreviewer http://bitbucket.org/denilsonsa/fontpreviewer">
 <title>Font list</title>
 <style type="text/css">
 html, body {
@@ -106,33 +128,40 @@ tr.second {
 }
 </style>
 </head><body>
+''')
 
-<table>
+    html_file.write('<table>\n\n'
+        '<!--<thead>\n'
+        '<tr class="first"><th colspan="3">Font name</th><th rowspan="2">{text}</th></tr>\n'
+        '<tr class="second"><th>filename</th><th>weight</th><th>italic</th></tr>\n'
+        '</thead>-->\n\n'
+        '<tbody>\n\n'.format(
+            text=text
+        )
+    )
 
-<!--<thead>
-<tr class="first"><th colspan="3">Font name</th><th rowspan="2">"""+text+"""</th></tr>
-<tr class="second"><th>filename</th><th>weight</th><th>italic</th></tr>
-</thead>-->
-
-<tbody>
-
-""")
     for e in metadata:
-        pngfile = os.path.splitext(e.filename)[0] + ".png"
-        if e.italic:
-            italic = "Italic"
-        else:
-            italic = ""
+        pngfilename = os.path.splitext(e.filename)[0] + '.png'
+        italic = 'Italic' if e.italic else ''
+
         html_file.write(
-            '<tr class="first"><td colspan="3">%s</td><td rowspan="2"><img src="%s" alt="" title="%s"></td></tr>\n' %
-            (e.name, pngfile, "%s - %s" % (e.filename, e.name))
-        )
-        html_file.write(
-            '<tr class="second"><td>%s</td><td>%d</td><td>%s</td></tr>\n\n' %
-            (e.filename, e.weight, italic)
+            '<tr class="first">'
+            '<td colspan="3">{name}</td>'
+            '<td rowspan="2"><img src="{pngfilename}" alt="" title="{filename} - {name}"></td>'
+            '</tr>\n'
+            '<tr class="second">'
+            '<td>{filename}</td><td>{weight}</td><td>{italic}</td>'
+            '</tr>\n\n'.format(
+                name=html(e.name),
+                filename=html(e.filename),
+                pngfilename=html(pngfilename),
+                weight=str(e.weight),
+                italic=e.italic,
+                text=text
+            )
         )
 
-    html_file.write("</tbody>\n\n</table>\n\n</body></html>\n")
+    html_file.write('</tbody>\n\n</table>\n\n</body>\n</html>\n')
 
 
 def print_help():
@@ -159,11 +188,11 @@ class ProgramOptions(object):
     """Holds the program options, after they are parsed by parse_options()"""
 
     def __init__(self):
-        self.text = "The Quick Brown Fox Jumped Over The Lazy Dog."
+        self.text = u'The Quick Brown Fox Jumped Over The Lazy Dog.'
         self.fontsize = 48
         self.antialias = True
         self.output_html = False
-        self.output_html_filename = ""
+        self.output_html_filename = ''
         self.args = []
 
 
@@ -174,37 +203,37 @@ def parse_options(argv, opt):
     try:
         opts, args = getopt.getopt(
             argv,
-            "s:t:aAho:",
-            ["size=", "text=", "help", "output="]
+            's:t:aAho:',
+            ['size=', 'text=', 'help', 'output=']
         )
     except getopt.GetoptError as e:
         sys.stderr.write(str(e) + "\n")
-        sys.stderr.write("Use --help for usage instructions.\n")
+        sys.stderr.write('Use --help for usage instructions.\n')
         sys.exit(2)
 
     for o, v in opts:
-        if o in ("-s", "--size"):
+        if o in ('-s', '--size'):
             opt.fontsize = int(v)
-        elif o == "-a":
+        elif o == '-a':
             opt.antialias = True
-        elif o == "-A":
+        elif o == '-A':
             opt.antialias = False
-        elif o in ("-t", "--text"):
-            opt.text = v
-        elif o in ("-o", "--output") and HAS_FONTTOOLS:
+        elif o in ('-t', '--text'):
+            opt.text = v.decode('utf-8')
+        elif o in ('-o', '--output') and HAS_FONTTOOLS:
             opt.output_html = True
             opt.output_html_filename = v
-        elif o in ("-h", "--help"):
+        elif o in ('-h', '--help'):
             print_help()
             sys.exit(0)
         else:
-            print "Invalid parameter: {0}".format(o)
-            print "Use --help for usage instructions."
+            print 'Invalid parameter: {0}'.format(o)
+            print 'Use --help for usage instructions.'
             sys.exit(2)
 
     opt.args = args
     if len(args) == 0:
-        sys.stderr.write("No files. Use --help for usage instructions.\n")
+        sys.stderr.write('No files. Use --help for usage instructions.\n')
         sys.exit(2)
 
 
@@ -212,7 +241,7 @@ def main():
     pygame.init()
 
     if not (pygame.font and pygame.font.get_init()):
-        sys.stderr.write("pygame.font has not been initialized. Do you have SDL_ttf?\n")
+        sys.stderr.write('pygame.font has not been initialized. Do you have SDL_ttf?\n')
         sys.exit(1)
 
     opt = ProgramOptions()
@@ -227,9 +256,9 @@ def main():
                 try:
                     metadata.append(FontMetadata(f))
                 except Exception as e:
-                    sys.stderr.write("Exception while getting metadata for '%s': %s\n" % (f, repr(e)))
+                    sys.stderr.write("Exception while getting metadata for '{0}': {1}\n".format(f, repr(e)))
         except Exception as e:
-            sys.stderr.write("Exception while rendering '%s': %s\n" % (f, repr(e)))
+            sys.stderr.write("Exception while rendering '{0}': {1}\n".format(f, repr(e)))
 
     if opt.output_html:
         try:
@@ -243,7 +272,7 @@ def main():
             if opt.output_html_filename != '-':
                 output_html_file.close()
         except IOError as e:
-            sys.stderr.write("IOError while writing '%s': %s\n" % (opt.output_html_filename, e))
+            sys.stderr.write("IOError while writing '{0}': {1}\n".format(opt.output_html_filename, str(e)))
 
 
 if __name__ == '__main__':
